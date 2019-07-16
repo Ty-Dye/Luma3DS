ecb4b2fafc5faee3b074eca0fd3f368c577bcc28
TyDye <dozar03@gmail.com>
2019-07-16 13:03:45 -0500

    readd screenshots

--- sysmodules/rosalina/source/menus.c
+++ sysmodules/rosalina/source/menus.c
@@ -54,6 +54,7 @@ Menu rosalinaMenu = {
         { "Miscellaneous options...", MENU, .menu = &miscellaneousMenu },
         { "Help Menu", MENU, .menu = &helpMenu },
         { "Credits", METHOD, .method = &RosalinaMenu_ShowCredits },
+        { "Screenshot", METHOD, .method = &RosalinaMenu_TakeScreenshot },
         { "Power off", METHOD, .method = &RosalinaMenu_PowerOff },
         { "Reboot", METHOD, .method = &RosalinaMenu_Reboot }
     }
@@ -139,7 +140,154 @@ void RosalinaMenu_PowerOff(void) // Soft shutdown.
         else if(pressed & BUTTON_B)
             return;
     }
-    while(!terminationRequest);
-}
-
+    while(!terminationRequest)
 // extern u8 framebufferCache[FB_BOTTOM_SIZE];
+extern u8 framebufferCache[FB_BOTTOM_SIZE];
+void RosalinaMenu_TakeScreenshot(void)
+{
+#define TRY(expr) if(R_FAILED(res = (expr))) goto end;
+
+    u64 total;
+    IFile file;
+    Result res;
+
+    char filename[64];
+
+    FS_Archive archive;
+    FS_ArchiveID archiveId;
+    s64 out;
+    bool isSdMode;
+
+    if(R_FAILED(svcGetSystemInfo(&out, 0x10000, 0x203))) svcBreak(USERBREAK_ASSERT);
+    isSdMode = (bool)out;
+
+    archiveId = isSdMode ? ARCHIVE_SDMC : ARCHIVE_NAND_RW;
+    Draw_Lock();
+    Draw_RestoreFramebuffer();
+
+    svcFlushEntireDataCache();
+
+    res = FSUSER_OpenArchive(&archive, archiveId, fsMakePath(PATH_EMPTY, ""));
+    if(R_SUCCEEDED(res))
+    {
+        res = FSUSER_CreateDirectory(archive, fsMakePath(PATH_ASCII, "/luma/screenshots"), 0);
+        if((u32)res == 0xC82044BE) // directory already exists
+            res = 0;
+        FSUSER_CloseArchive(archive);
+    }
+
+    u32 seconds, minutes, hours, days, year, month;
+    u64 milliseconds = osGetTime();
+    seconds = milliseconds/1000;
+    milliseconds %= 1000;
+    minutes = seconds / 60;
+    seconds %= 60;
+    hours = minutes / 60;
+    minutes %= 60;
+    days = hours / 24;
+    hours %= 24;
+
+    year = 1900; // osGetTime starts in 1900
+
+    while(true)
+    {
+        bool leapYear = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+        u16 daysInYear = leapYear ? 366 : 365;
+        if(days >= daysInYear)
+        {
+            days -= daysInYear;
+            ++year;
+        }
+        else
+        {
+            static const u8 daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            for(month = 0; month < 12; ++month)
+            {
+                u8 dim = daysInMonth[month];
+
+                if (month == 1 && leapYear)
+                    ++dim;
+
+                if (days >= dim)
+                    days -= dim;
+                else
+                    break;
+            }
+            break;
+        }
+    }
+    days++;
+    month++;
+
+    sprintf(filename, "/luma/screenshots/%04lu-%02lu-%02lu_%02lu-%02lu-%02lu.%03llu_top.bmp", year, month, days, hours, minutes, seconds, milliseconds);
+    TRY(IFile_Open(&file, archiveId, fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, filename), FS_OPEN_CREATE | FS_OPEN_WRITE));
+    Draw_CreateBitmapHeader(framebufferCache, 400, 240);
+
+    for(u32 y = 0; y < 120; y++)
+        Draw_ConvertFrameBufferLine(framebufferCache + 54 + 3 * 400 * y, true, true, y);
+
+    TRY(IFile_Write(&file, &total, framebufferCache, 54 + 3 * 400 * 120, 0));
+
+    for(u32 y = 120; y < 240; y++)
+        Draw_ConvertFrameBufferLine(framebufferCache + 3 * 400 * (y - 120), true, true, y);
+
+    TRY(IFile_Write(&file, &total, framebufferCache, 3 * 400 * 120, 0));
+    TRY(IFile_Close(&file));
+
+    sprintf(filename, "/luma/screenshots/%04lu-%02lu-%02lu_%02lu-%02lu-%02lu.%03llu_bot.bmp", year, month, days, hours, minutes, seconds, milliseconds);
+    TRY(IFile_Open(&file, archiveId, fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, filename), FS_OPEN_CREATE | FS_OPEN_WRITE));
+    Draw_CreateBitmapHeader(framebufferCache, 320, 240);
+
+    for(u32 y = 0; y < 120; y++)
+        Draw_ConvertFrameBufferLine(framebufferCache + 54 + 3 * 320 * y, false, true, y);
+
+    TRY(IFile_Write(&file, &total, framebufferCache, 54 + 3 * 320 * 120, 0));
+
+    for(u32 y = 120; y < 240; y++)
+        Draw_ConvertFrameBufferLine(framebufferCache + 3 * 320 * (y - 120), false, true, y);
+
+    TRY(IFile_Write(&file, &total, framebufferCache, 3 * 320 * 120, 0));
+    TRY(IFile_Close(&file));
+
+    if((GPU_FB_TOP_FMT & 0x20) && (Draw_GetCurrentFramebufferAddress(true, true) != Draw_GetCurrentFramebufferAddress(true, false)))
+    {
+        sprintf(filename, "/luma/screenshots/%04lu-%02lu-%02lu_%02lu-%02lu-%02lu.%03llu_top_right.bmp", year, month, days, hours, minutes, seconds, milliseconds);
+        TRY(IFile_Open(&file, archiveId, fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, filename), FS_OPEN_CREATE | FS_OPEN_WRITE));
+        Draw_CreateBitmapHeader(framebufferCache, 400, 240);
+
+        for(u32 y = 0; y < 120; y++)
+            Draw_ConvertFrameBufferLine(framebufferCache + 54 + 3 * 400 * y, true, false, y);
+
+        TRY(IFile_Write(&file, &total, framebufferCache, 54 + 3 * 400 * 120, 0));
+
+        for(u32 y = 120; y < 240; y++)
+            Draw_ConvertFrameBufferLine(framebufferCache + 3 * 400 * (y - 120), true, false, y);
+
+        TRY(IFile_Write(&file, &total, framebufferCache, 3 * 400 * 120, 0));
+        TRY(IFile_Close(&file));
+    }
+
+end:
+    IFile_Close(&file);
+    svcFlushEntireDataCache();
+    Draw_SetupFramebuffer();
+    Draw_ClearFramebuffer();
+    Draw_FlushFramebuffer();
+    Draw_Unlock();
+
+    do
+    {
+        Draw_Lock();
+        Draw_DrawString(10, 10, COLOR_TITLE, "Screenshot");
+        if(R_FAILED(res))
+            Draw_DrawFormattedString(10, 30, COLOR_WHITE, "Operation failed (0x%08lx).", (u32)res);
+        else
+            Draw_DrawString(10, 30, COLOR_WHITE, "Operation succeeded.");
+
+        Draw_FlushFramebuffer();
+        Draw_Unlock();
+    }
+    while(!(waitInput() & BUTTON_B) && !terminationRequest);
+
+#undef TRY
+}
